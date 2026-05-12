2689387af70a676a92b5ce616b5433f6a97c9e1b
BRGOKA <bgoudjal3@gmail.com>
2026-05-13 00:19:43 +0100

    added the app layout and styled it

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,8 +1,16 @@
+import styled from "styled-components"
+
+const StyledSideBar = styled.aside`
+background-color: var(--color-grey-0);
+padding: 3.2rem 2.4rem;
+border-right: 1px solid var(--color-grey-300);
+grid-row: 1/-1;
+`
 function SideBar() {
   return (
-    <aside>
+    <StyledSideBar>
       SideBar
-    </aside>
+    </StyledSideBar>
   )
 }
 
