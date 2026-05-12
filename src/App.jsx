242806c7f7d596eb912bd85362df1606b2193f87
BRGOKA <bgoudjal3@gmail.com>
2026-05-12 20:28:00 +0100

    refactored the button and input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import styled from "styled-components"
 import GlobalStyles from "./styles/GlobalStyles"
+import Button from "./ui/Button"
 
 const H1=styled.h1`font-size:30px;
 font-weight:600;
 `
-const Button = styled.button`
-font-size: 1.4rem;
-padding: 1.2rem 1.6rem;
-font-weight: 500;
-border: none;
-border-radius: 7px;
-background-color: blueviolet;
-color: wheat;
-`
+
 
 const StyledApp = styled.div`
 padding: 1.6rem 1.5rem;
@@ -23,10 +16,10 @@ function App() {
   return (
     <>
     <GlobalStyles/>
-    
     <StyledApp>
       <H1>The Wiled oasis</H1>
       <Button>Check-In</Button>
+      <Input type="number" placeholder="number of guests" />
     </StyledApp>
     </>
   )
