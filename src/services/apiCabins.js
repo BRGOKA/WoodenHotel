ef60821683e72c9cd21e5c0d06524c65c6a844cb
BRGOKA <bgoudjal3@gmail.com>
2026-07-11 18:27:03 +0100

    ls gooo fixed the image upload bro i was missing [0] XD fk i wasted 2 days for this

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -33,7 +33,7 @@ export async function createEditCabin({ data: cabinData, id }) {
   // image upload
   const { error: storageError } = await supabase.storage
     .from("cabin-images")
-    .upload(imageName, cabinData.image);
+    .upload(imageName, cabinData.image[0]);
 
   if (storageError) {
     console.error(storageError);
