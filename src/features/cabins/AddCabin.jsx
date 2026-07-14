4c8316f42b9de3da6ef8f534a4f64b05e9122791
BRGOKA <bgoudjal3@gmail.com>
2026-07-14 22:29:04 +0100

    turned AddCabin to a compouned component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,18 +1,28 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open>
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window>
-        <CreateCabinForm />
-      </Modal.Window>
-    </Modal>
+    <>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+
+      {/* <Modal>
+        <Modal.Open opens="table">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="table">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal> */}
+    </>
   );
 }
 
