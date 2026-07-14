1ab8d8a0927d179088ce05f8bbee0eda31d009b1
BRGOKA <bgoudjal3@gmail.com>
2026-07-14 20:43:02 +0100

    adding add cabin modal component

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Button from "../../ui/Button";
+import CreateCabinForm from "./CreateCabinForm";
+
+function AddCabin() {
+  const [showForm, setShowForm] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setShowForm((showForm) => !showForm)}>
+        add new cabin
+      </Button>
+      {showForm && <CreateCabinForm />}
+    </div>
+  );
+}
+
+export default AddCabin;
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,8 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
-import { useState } from "react";
-import Button from "../ui/Button";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -15,10 +11,6 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
-        <Button onClick={() => setShowForm((showForm) => !showForm)}>
-          add new cabin
-        </Button>
-        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
