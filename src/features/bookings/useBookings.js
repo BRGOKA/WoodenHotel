09e8c03230bc1d21512a10eb1653ddf673fcc1d3
BRG <bgoudjal3@gmail.com>
2026-07-29 20:08:24 +0100

    done with prefetching next and prev page

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,6 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { bookingsPerPage } from "../../utils/constants";
 
 function useBookings() {
   const queryClient = useQueryClient();
@@ -30,11 +31,19 @@ function useBookings() {
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
   // PREFETCHING
-  queryClient.prefetchQuery({
-    queryKey: ["bookings", filter, sortBy, page + 1],
-    queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
-  });
+  const pageCount = Math.ceil(count / bookingsPerPage);
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
   return { isLoading, bookings, error, count };
 }
 
