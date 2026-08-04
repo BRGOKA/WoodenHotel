5bfb66a2bf70af6d2555b50e1a71cb47ca801567
BRG <bgoudjal3@gmail.com>
2026-08-04 18:50:18 +0100

    implemented login function and protected routes + autherization

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
   const navigate = useNavigate();
+
   const { mutate: login, isLoading: isLoginIn } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: () => {
--- src/ui/ProtectedRout.jsx
+++ src/ui/ProtectedRout.jsx
@@ -2,9 +2,11 @@ import { useNavigate } from "react-router-dom";
 import { useUser } from "../features/authentication/useUser";
 import Spinner from "./Spinner";
 import styled from "styled-components";
+import { useEffect } from "react";
 
 const FullPage = styled.div`
   height: 100vh;
+  background-color: var(--color-grey-500);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -12,14 +14,26 @@ const FullPage = styled.div`
 
 function ProtectedRout({ children }) {
   const navigate = useNavigate();
-  // TODO:Load autherized users
-  const { user, isLoading } = useUser();
 
-  // TODO:while loading show a spinner
-  if (isLoading) return <Spinner />;
+  // TODO:Load autherized users
+  const { isAuthenticated, isLoading } = useUser();
+  console.log(isAuthenticated);
 
   // TODO: if no authed user redirect to log in page
-  if (!user) Navigate("/login");
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading],
+  );
+
+  // TODO:while loading show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />;
+      </FullPage>
+    );
 
   // TODO:
   return children;
