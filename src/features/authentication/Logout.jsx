ccdd8713b3be99ec98e18d8eeaf0174450dcb288
BRG <bgoudjal3@gmail.com>
2026-08-04 19:20:35 +0100

    adde logout button and logic but for some reason its not clearing the user from the cash

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,18 @@
+import {
+  HiArrowRightOnRectangle,
+  HiMiniArrowTopRightOnSquare,
+} from "react-icons/hi2";
+import ButtonIcon from "../../ui/ButtonIcon";
+import { useLogout } from "./useLogout";
+import SpinnerMini from "../../ui/SpinnerMini";
+
+function Logout() {
+  const { logout, isLoginOut } = useLogout();
+  return (
+    <ButtonIcon onClick={logout} disabled={isLoginOut}>
+      {isLoginOut ? <SpinnerMini /> : <HiArrowRightOnRectangle />}
+    </ButtonIcon>
+  );
+}
+
+export default Logout;
--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading: isLoginIn } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: () => {
-      navigate("/dashboard");
+      queryClient.removeQueries();
+      navigate("/dashboard", { replace: true });
     },
     onError: (err) => toast.error("Provided email or password are incorrect"),
   });
