6556c8d52776ea833578533cbf9396dfb8797dee
BRG <bgoudjal3@gmail.com>
2026-08-05 11:47:24 +0100

    added updating user data logic and connections to the db

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 //
 export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -40,4 +40,30 @@ export async function logout() {
   const { error } = await supabase.auth.signOut();
 
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ fullName, password, avatar }) {
+  // TODO: update user fullName or password
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  // TODO: upload avatar image
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+  const { error: avatarStorageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+  if (avatarStorageError) throw new Error(avatarStorageError.message);
+
+  // TODO: update avatar in the db
+  const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+  if (error2) throw new Error(error2.message);
+  return updatedUser;
 }
