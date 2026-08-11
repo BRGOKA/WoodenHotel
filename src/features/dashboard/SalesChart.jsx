e69fe06461ccdae2dacf1d5a19ff343582380dd5
BRG <bgoudjal3@gmail.com>
2026-08-11 21:38:23 +0100

    done styling my sales chart

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -11,6 +11,7 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+import { useDarkMode } from "../../context/DarkModeContext";
 
 const StyledSalesChart = styled(DashboardBox)`
   grid-column: 1 / -1;
@@ -54,36 +55,55 @@ const fakeData = [
   { label: "Feb 06", totalSales: 1450, extrasSales: 400 },
 ];
 
-const isDarkMode = true;
-const colors = isDarkMode
-  ? {
-      totalSales: { stroke: "#4f46e5", fill: "#4f46e5" },
-      extrasSales: { stroke: "#22c55e", fill: "#22c55e" },
-      text: "#e5e7eb",
-      background: "#18212f",
-    }
-  : {
-      totalSales: { stroke: "#4f46e5", fill: "#c7d2fe" },
-      extrasSales: { stroke: "#16a34a", fill: "#dcfce7" },
-      text: "#374151",
-      background: "#fff",
-    };
-
 function SalesChart() {
+  const { isDarkMode } = useDarkMode();
+  const colors = isDarkMode
+    ? {
+        totalSales: { stroke: "#4f46e5", fill: "#4f46e5" },
+        extrasSales: { stroke: "#22c55e", fill: "#22c55e" },
+        text: "#e5e7eb",
+        background: "#18212f",
+      }
+    : {
+        totalSales: { stroke: "#4f46e5", fill: "#c7d2fe" },
+        extrasSales: { stroke: "#16a34a", fill: "#dcfce7" },
+        text: "#374151",
+        background: "#fff",
+      };
   return (
     <StyledSalesChart>
       <Heading as="h2">Sales</Heading>{" "}
       <ResponsiveContainer height={300} width="100%">
         <AreaChart data={fakeData}>
           <CartesianGrid />
-          <Tooltip />
-          <XAxis dataKey="label" />
-          <YAxis unit="$" />
+          <Tooltip contentStyle={{ background: colors.background }} />
+          <XAxis
+            dataKey="label"
+            tickLine={{ stroke: colors.text }}
+            tick={{ fill: colors.text }}
+          />
+          <YAxis
+            unit="$"
+            tickLine={{ stroke: colors.text }}
+            tick={{ fill: colors.text }}
+          />
           <Area
             dataKey="totalSales"
             type="monotone"
-            stroke="red"
-            fill="orange"
+            stroke={colors.totalSales.stroke}
+            fill={colors.totalSales.fill}
+            strokeWidth={2}
+            name="Total Sales"
+            unit={"$"}
+          />
+          <Area
+            dataKey="extrasSales"
+            type="monotone"
+            stroke={colors.extrasSales.stroke}
+            fill={colors.extrasSales.fill}
+            strokeWidth={2}
+            name="Total Sales"
+            unit={"$"}
           />
         </AreaChart>
       </ResponsiveContainer>
